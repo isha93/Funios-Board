85b495bf9d747ccaa54fc520fbef3960c23e4599
Danang Wijaya <danangwijaya750@gmail.com>
2022-07-28 19:05:32 +0700

    feat: Adding LogginScreen

--- Funios-Board/Funios-Board/AppDelegate.swift
+++ Funios-Board/Funios-Board/AppDelegate.swift
@@ -9,14 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window=UIWindow(frame: UIScreen.main.bounds)
+        let loginViewController = LoginViewController() as UIViewController
+        let nav = UINavigationController(rootViewController: loginViewController)
+        nav.navigationBar.isTranslucent=false
+        window?.rootViewController=nav
+        window?.makeKeyAndVisible()
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
---
+++ Funios-Board/Funios-Board/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Funios-Board
+//
+//  Created by Danang Wijaya on 24/07/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
