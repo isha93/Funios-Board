3e014065494f0c1c392e229c0565aa8b43d1bc06
Caroline Chan <carolinechan304@gmail.com>
2022-07-28 15:48:40 +0700

    Feat: Add a Container or Form for Login

--- Funios-Board/Funios-Board/ViewController.swift
+++ Funios-Board/Funios-Board/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ViewForLogin: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        editViewForLogin()
     }
 
+    func editViewForLogin() {
+        ViewForLogin.layer.cornerRadius = 50
+    }
 
 }
 
