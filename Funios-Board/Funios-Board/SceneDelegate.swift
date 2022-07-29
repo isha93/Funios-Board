c91fa3fcab504b7791b1b2b5bb50d53c111ae5e4
Danang Wijaya <danangwijaya750@gmail.com>
2022-07-29 09:14:01 +0700

    feat : edit entry point SceneDelegate, add ui design in LoginScreen.xib

--- Funios-Board/Funios-Board/SceneDelegate.swift
+++ Funios-Board/Funios-Board/SceneDelegate.swift
@@ -10,18 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var navController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         if let windowScene = (scene as? UIWindowScene){
-            window=UIWindow(windowScene: windowScene)
-            let loginViewController = LoginViewController() as UIViewController
-            let nav = UINavigationController(rootViewController: loginViewController)
-            nav.navigationBar.isTranslucent=false
-            window?.rootViewController=nav
-            window?.makeKeyAndVisible()
+            let window = UIWindow(windowScene: windowScene)
+            navController = UINavigationController(rootViewController: LoginViewController())
+            window.rootViewController = navController
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
 
