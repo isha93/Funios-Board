a29cd04759505b24c68510c7378f07a085c93f3a
suryaharahap <suryaharahap18@gmail.com>
2022-07-30 03:21:01 +0700

    Fix: Set up cardView to cornerRadius

--- Funios-Board/Funios-Board/Login/LoginViewController.swift
+++ Funios-Board/Funios-Board/Login/LoginViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
+  
+  @IBOutlet var cardView: UIView!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLayout()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// MARK: - Set up cardView
+extension LoginViewController {
+    func setupLayout(){
+      cardView.layer.cornerRadius = 35
+    }
+}
+
+
