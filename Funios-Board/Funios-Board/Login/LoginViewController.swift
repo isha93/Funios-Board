0e17104f5e9a61996b9c8c899402e393d33bec77
mochammad jamaluddin al fadli <jamaluddinalfadli97@gmail.com>
2022-08-04 23:26:40 +0700

    Fix: Improve code to implementation hide password and show password

--- Funios-Board/Funios-Board/Login/LoginViewController.swift
+++ Funios-Board/Funios-Board/Login/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
     var showBtnPassword: Bool = false
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var passwordHideButton: UIButton!
     
@@ -21,6 +23,16 @@ class LoginViewController: UIViewController {
 
     @IBAction func changePasswordHidden(_ sender: Any) {
         showBtnPassword = !showBtnPassword
+        
+        if(showBtnPassword == true){
+            let imageView = UIImage(systemName: "eye")!
+            passwordTextField.isSecureTextEntry = false
+            passwordHideButton.setImage(imageView, for: .normal)
+        } else{
+            let imageView = UIImage(systemName: "eye.slash")!
+            passwordTextField.isSecureTextEntry = true
+            passwordHideButton.setImage(imageView, for: .normal)
+        }
         
     }
     
