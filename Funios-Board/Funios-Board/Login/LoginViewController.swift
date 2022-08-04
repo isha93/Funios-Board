e97afd18c95e533ad11e4193e618161dde382ae9
mochammad jamaluddin al fadli <jamaluddinalfadli97@gmail.com>
2022-08-04 23:16:41 +0700

    Fix: Improve code cardView to cornerRadius

--- Funios-Board/Funios-Board/Login/LoginViewController.swift
+++ Funios-Board/Funios-Board/Login/LoginViewController.swift
@@ -8,22 +8,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    var showBtnPassword: Bool = false
 
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var passwordHideButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        settingCardView()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changePasswordHidden(_ sender: Any) {
+        showBtnPassword = !showBtnPassword
+        
     }
-    */
-
+    
+    func settingCardView(){
+        cardView.layer.cornerRadius = 33
+    }
 }
