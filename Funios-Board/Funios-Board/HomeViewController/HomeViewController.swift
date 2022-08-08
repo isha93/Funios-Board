7c01100b2720b7e22ca705ef0355067871a31ade
Caroline Chan <carolinechan304@gmail.com>
2022-08-08 16:58:10 +0700

    Feat: add transfer and top up button

--- Funios-Board/Funios-Board/HomeViewController/HomeViewController.swift
+++ Funios-Board/Funios-Board/HomeViewController/HomeViewController.swift
@@ -12,14 +12,22 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var ProfileImageView: UIImageView!
     @IBOutlet weak var CardView: UIView!
     @IBOutlet weak var NotificationButton: UIButton!
+    @IBOutlet weak var TransferButton: UIButton!
+    @IBOutlet weak var TopUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         editCardViewContent()
+        buttonsCornerRadius()
     }
     
     func editCardViewContent() {
         CardView.layer.cornerRadius = 20
-        ProfileImageView.layer.cornerRadius = 10
+        ProfileImageView.layer.cornerRadius = 20
+    }
+    
+    func buttonsCornerRadius() {
+        TransferButton.layer.cornerRadius = 15
+        TopUpButton.layer.cornerRadius = 15
     }
 }
