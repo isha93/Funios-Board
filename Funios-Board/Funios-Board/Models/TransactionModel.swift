a590773f4975663e1a6ceff04b2aa86b6778f8fa
Caroline Chan <carolinechan304@gmail.com>
2022-08-08 17:25:08 +0700

    Feat: add transaction model

---
+++ Funios-Board/Funios-Board/Models/TransactionModel.swift
@@ -0,0 +1,25 @@
+//
+//  TransactionModel.swift
+//  Funios-Board
+//
+//  Created by Caroline Chan on 08/08/22.
+//
+
+import Foundation
+import UIKit
+
+struct TransactionModel {
+    let image : UIImage
+    let title : String
+    let description : String
+    let amount : Int
+}
+
+//MARK: dummy transaction data to show in table view
+let dummyTransactionData = [
+    TransactionModel(image: UIImage(named: "Profile")!, title: "Samuel Sushi", description: "Transfer", amount: 50000),
+    TransactionModel(image: UIImage(named: "Spotify")!, title: "Spotify", description: "Subscription", amount: -49000),
+    TransactionModel(image: UIImage(named: "Youtube")!, title: "Youtube", description: "Subscription", amount: -149000),
+    TransactionModel(image: UIImage(named: "Profile")!, title: "Budi Martabak", description: "Transfer", amount: 100000),
+    TransactionModel(image: UIImage(named: "Profile")!, title: "Ani Ramen", description: "Transfer", amount: 10000)
+]
