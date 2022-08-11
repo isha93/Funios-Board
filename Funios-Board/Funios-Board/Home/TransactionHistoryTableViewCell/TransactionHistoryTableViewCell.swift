a4996c5b42c47bf0bc07e1764b9ccab38075cab5
adedwi1808 <adedwip1808@gmail.com>
2022-08-11 21:22:44 +0700

    ADD: Menambahkan TransactionHistoryTableViewCell

---
+++ Funios-Board/Funios-Board/Home/TransactionHistoryTableViewCell/TransactionHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionHistoryTableViewCell.swift
+//  Funios-Board
+//
+//  Created by Ade Dwi Prayitno on 11/08/22.
+//
+
+import UIKit
+
+class TransactionHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
