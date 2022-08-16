b251ec231496505943a2d7b16c98f79c995fcda9
Azri <azrimuhammad5@gmail.com>
2022-08-16 22:43:37 +0700

    ADD: Implement UI on HomeViewController

---
+++ Funios-Board/Funios-Board/UI/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Funios-Board
+//
+//  Created by Azri Muhammad on 13/08/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Funios-Board/Funios-Board/UI/Home/TableViewCell/HomeTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  HomeTableViewCell.swift
+//  Funios-Board
+//
+//  Created by Azri Muhammad on 14/08/22.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+}
