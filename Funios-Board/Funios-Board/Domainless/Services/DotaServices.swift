78210b85890709877196d230e90acd122f245b23
adedwi1808 <adedwip1808@gmail.com>
2022-09-20 23:16:10 +0700

    ADD: Menambahkan DotaServiceProtocol dan Class DotaServices

--- Funios-Board/Funios-Board/Domainless/Services/DotaServices.swift
+++ Funios-Board/Funios-Board/Domainless/Services/DotaServices.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol DotaServicesProtocol: AnyObject {
+    var networker: NetworkerProtocol { get }
+    func getHeroes(endPoint: NetworkFactory) async throws -> DotaModel
+}
+
+final class DotaServices: DotaServicesProtocol {
+    var networker: NetworkerProtocol
+    
+    init(networker: NetworkerProtocol = Networker()){
+        self.networker = networker
+    }
+    
+    func getHeroes(endPoint: NetworkFactory) async throws -> DotaModel {
+        return try await networker.taskAsync(type: DotaModel.self, endPoint: endPoint, isMultipart: false)
+    }
+}
