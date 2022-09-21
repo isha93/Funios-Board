fc3aca9b956a819a489880a027f28fa1ac7f32f0
adedwi1808 <adedwip1808@gmail.com>
2022-09-21 23:57:07 +0700

    ADD: Menambahkan ListDotaHeroTableViewCell

---
+++ Funios-Board/Funios-Board/Presentation/List Dota Hero/ListDotaHeroTableViewCell/ListDotaHeroTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListDotaHeroTableViewCell.swift
+//  Funios-Board
+//
+//  Created by Ade Dwi Prayitno on 21/09/22.
+//
+
+import UIKit
+
+class ListDotaHeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
